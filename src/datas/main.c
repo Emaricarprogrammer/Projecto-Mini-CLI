dd7dbf99a6b83ea24a35eb6548311a4b77752625
Emanuel <=>
2026-01-19 20:00:30 +0100

    Novos conteudos

--- src/datas/main.c
+++ src/datas/main.c
@@ -14,99 +14,108 @@ int main(void)
     char finalPath[512];
     int groupSize;
 
-    printf("=================================");
-    printf("\n\tGestão da Turma");
-    printf("\n=================================\n");
-    printf("\n 1 - Enviar conteúdos");
-    printf("\n 2 - Baixar de conteúdos");
-    printf("\n 3 - Gerar grupos aleatórios");
-    printf("\n 4 - Eleger delegado(a)");
-    printf("\n 5 - Sair\n");
-    printf("sua opção:");
-    scanf("%d", &option);
-
-    if (option == 1)
+    do
     {
         printf("=================================");
-        printf("\n Cadeiras disponiveis ");
+        printf("\n\tGestão da Turma");
         printf("\n=================================\n");
-        printf("\n 1- Programação 1");
-        printf("\n 2- Comunicação escrita");
-        printf("\n 3- Análise matemática\n");
-        printf("\n 4- Lógica matemática\n");
-        printf("\n 5- INglês técnico\n");
-        printf("\n 6- Mic\n");
-        printf("\n 7- ICC\n");
-        printf("\n 8 - Sair\n");
+        printf("\n 1 - Enviar conteúdos");
+        printf("\n 2 - Baixar conteúdos");
+        printf("\n 3 - Gerar grupos aleatórios");
+        printf("\n 4 - Eleger delegado(a)");
+        printf("\n 5 - Sair\n");
+        printf("Sua opção: ");
+        scanf("%d", &option);
 
-        printf("Sua opção:");
-        scanf("%d", &branch);
-
-        if(branch == 1)
+        switch (option)
         {
+        case 1:
             printf("=================================");
-            printf("Digite o nome do ficheiro a enviar (teste.txt):");
-            scanf("%s", relativePath);
+            printf("\n Cadeiras disponiveis ");
+            printf("\n=================================\n");
+            printf("\n 1- Programação 1");
+            printf("\n 2- Comunicação escrita");
+            printf("\n 3- Análise matemática");
+            printf("\n 4- Lógica matemática");
+            printf("\n 5- Inglês técnico");
+            printf("\n 6- Mic");
+            printf("\n 7- ICC");
+            printf("\n 8 - Sair");
 
-            snprintf(finalPath, sizeof(finalPath), "../Topics/P1/%s", relativePath);
-            if(access(finalPath, F_OK) != 0)
+            printf("\nSua opção: ");
+            scanf("%d", &branch);
+
+            if (branch == 1)
             {
-                printf("❌ Ficheiro não encontrado no directorio: %s\n", finalPath);
-                return 1;
-            }
-            makePushToTheRepo("P1", "Novos conteudos", finalPath);       
-        }
-        else
-        {
-            printf("Opção inválida!\n");
-        }
-    }
-    else if (option == 2)
-    {
-        printf("=================================");
-        printf("\n Cadeiras disponiveis ");
-        printf("\n=================================\n");
-        printf("\n 1- Programação 1");
-        printf("\n 2- Comunicação escrita");
-        printf("\n 3- Análise matemática\n");
-        printf("\n 4- Lógica matemática\n");
-        printf("\n 5- INglês técnico\n");
-        printf("\n 6- Mic\n");
-        printf("\n 7- ICC\n");
-        printf("\n 8 - Sair\n");
+                printf("Digite o nome do ficheiro a enviar (teste.txt): ");
+                scanf("%s", relativePath);
 
-        printf("\nSua opção:\n");
-        scanf("%d", &branch);
-        if(branch == 1)
-        {
-            system("clear");
+                snprintf(finalPath, sizeof(finalPath), "../Topics/P1/%s", relativePath);
+
+                if (access(finalPath, F_OK) != 0)
+                {
+                    printf("❌ Ficheiro não encontrado: %s\n", finalPath);
+                }
+                else
+                {
+                    makePushToTheRepo("P1", "Novos conteudos", finalPath);
+                }
+            }
+            else if (branch != 8)
+            {
+                printf("Opção inválida!\n");
+            }
+            break;
+
+        case 2:
             printf("=================================");
-            downloadTopics("P1");
-        }
-        else
-        {
+            printf("\n Cadeiras disponiveis ");
+            printf("\n=================================\n");
+            printf("\n 1- Programação 1");
+            printf("\n 2- Comunicação escrita");
+            printf("\n 3- Análise matemática");
+            printf("\n 4- Lógica matemática");
+            printf("\n 5- Inglês técnico");
+            printf("\n 6- Mic");
+            printf("\n 7- ICC");
+            printf("\n 8 - Sair\n");
+
+            printf("\nSua opção: ");
+            scanf("%d", &branch);
+
+            if (branch == 1)
+            {
+                system("clear");
+                downloadTopics("P1");
+            }
+            else if (branch != 8)
+            {
+                printf("Opção inválida!\n");
+            }
+            break;
+
+        case 3:
+            printf("Informe a quantidade de elementos para cada grupo: ");
+            scanf("%d", &groupSize);
+            generateRandomGroups("../../alunos.txt", groupSize);
+            break;
+
+        case 4:
+            printf("Delegado(a) eleito(a) com sucesso!\n");
+            break;
+
+        case 5:
+            printf("Saindo do programa...\n");
+            break;
+
+        default:
             printf("Opção inválida!\n");
         }
-    }
-    else if (option == 3)
-    {
+
+        sleep(1);
         system("clear");
-        printf("=================================\n");
-        printf("Informe a quantidade de elementos para cada grupo: ");
-        scanf("%d", &groupSize);
-        generateRandomGroups("../../alunos.txt", groupSize);
-    }
-    else if (option == 4)
-    {
-        printf("Delegado(a) eleito(a) com sucesso!\n");
-    }
-    else if (option == 5)
-    {
-        printf("Saindo do programa...\n");
-    }
-    else
-    {
-        printf("Opção inválida!\n");
-    }
+
+    } while (option != 5);
+
     return 0;
 }
